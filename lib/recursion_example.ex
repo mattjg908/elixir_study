6fa29fb6c67d43cb8f87db7569fb9618318a0b9d
Matt Galvin <matt.galvin@dockyard.com>
2019-11-21 19:53:39 +0100

    Use tabs on recursion_example

--- lib/recursion_example.ex
+++ lib/recursion_example.ex
@@ -1,36 +1,36 @@
 defmodule RecursionExample do
   @moduledoc """
-	Documentation for RecursionExample.
-	"""
+  Documentation for RecursionExample.
+  """
 
-	@doc """
-	RecursionExample.sum
+  @doc """
+  RecursionExample.sum
 
-	## Parameters
-		-n Natural Number
+  ## Parameters
+    -n Natural Number
 
-	## Examples
+  ## Examples
 
-			iex> RecursionExample.sum(3)
-			6
-	"""
+      iex> RecursionExample.sum(3)
+      6
+  """
 
-	def sum(1), do: 1
-	def sum(n), do: n + sum(n - 1)
+  def sum(1), do: 1
+  def sum(n), do: n + sum(n - 1)
 
-	@doc """
-	RecursionExample.gcd
+  @doc """
+  RecursionExample.gcd
 
-	## Parameters
-		-x Natural Number
-		-y Natural Number
+  ## Parameters
+    -x Natural Number
+    -y Natural Number
 
-	## Examples
+  ## Examples
 
-			iex> RecursionExample.gcd(10,20)
-			10
-	"""
+      iex> RecursionExample.gcd(10,20)
+      10
+  """
 
-	def gcd(x, 0), do: x
-	def gcd(x, y), do: gcd(y, rem(x,y))
+  def gcd(x, 0), do: x
+  def gcd(x, y), do: gcd(y, rem(x,y))
 end
