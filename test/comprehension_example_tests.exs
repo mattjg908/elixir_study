4e0c152e6897c42b30499bae94b4f1250edcceb9
Matt Galvin <matthewgalvin908@gmail.com>
2020-10-17 21:09:54 +0200

    Try out a new syntax for setup block

--- test/comprehension_example_tests.exs
+++ test/comprehension_example_tests.exs
@@ -10,18 +10,19 @@ defmodule ComprehensionExampleTest do
 
   describe "calc_tax/2" do
     setup do
-      orders = [
-        [id: 123, ship_to: :NC, net_amount: 100.00],
-        [id: 124, ship_to: :OK, net_amount: 35.50],
-        [id: 125, ship_to: :TX, net_amount: 24.00],
-        [id: 126, ship_to: :TX, net_amount: 44.80],
-        [id: 127, ship_to: :NC, net_amount: 25.00],
-        [id: 128, ship_to: :MA, net_amount: 10.00],
-        [id: 129, ship_to: :CA, net_amount: 102.00],
-        [id: 130, ship_to: :NC, net_amount: 50.00]
+      [
+        orders: [
+          [id: 123, ship_to: :NC, net_amount: 100.00],
+          [id: 124, ship_to: :OK, net_amount: 35.50],
+          [id: 125, ship_to: :TX, net_amount: 24.00],
+          [id: 126, ship_to: :TX, net_amount: 44.80],
+          [id: 127, ship_to: :NC, net_amount: 25.00],
+          [id: 128, ship_to: :MA, net_amount: 10.00],
+          [id: 129, ship_to: :CA, net_amount: 102.00],
+          [id: 130, ship_to: :NC, net_amount: 50.00]
+        ],
+        tax_rates: [NC: 0.075, TX: 0.08]
       ]
-
-      {:ok, tax_rates: [NC: 0.075, TX: 0.08], orders: orders}
     end
 
     test "Returns a list of orders with an extra field, total_amount, which is equal to the order's net_amount plus sales tax`", %{tax_rates: tax_rates, orders: orders} do
